b614a5aeda893204157f68786e24bd40ec82a2de
ucyo <cayoglu@me.com>
2019-05-03 09:52:47 +0200

    [feature] add cmake build script

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+use cmake::Config;
+
+fn main() {
+    let dst = Config::new("libdivsufsort")
+        .define("BUILD_EXAMPLES", "ON")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_DIVSUFSORT64", "ON")
+        .define("CMAKE_INSTALL_LIBDIR", env::var("OUT_DIR").unwrap())
+        .build();
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-lib=static=divsufsort64");
+}
